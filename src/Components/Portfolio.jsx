8b9cd7a5d8c4da5b6c56683c9eaef9c649f80b46
Reham-Abdul-Aleem <104021064+Reham-Abdul-Aleem@users.noreply.github.com>
2023-12-22 17:11:12 +0000

    Corrected filetype name

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/portfolio.png";
+import image from "../images/portfolio.jpg";
 
 const imageAltText = "An image of a desk with a laptop, notebook and coffee mug.";
 
